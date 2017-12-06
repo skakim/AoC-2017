7264c007400cf133b1d129effc2b6eb78fe6d327
Gabriel Moita <gmmoita@terra.com.br>
2017-12-06 03:13:10 -0200

    Day 6: Memory Reallocation

---
+++ p6.py
@@ -0,0 +1,34 @@
+banks = list(map(int,input().split("\t")))
+
+#part 1
+seen = []
+acc1 = 0
+while tuple(banks) not in seen:
+  acc1 += 1
+  seen.append(tuple(banks))
+  i = banks.index(max(banks))
+  temp = banks[i]
+  banks[i] = 0
+  i = (i+1)%(len(banks))
+  while(temp != 0):
+    banks[i] += 1
+    temp -= 1
+    i = (i+1)%(len(banks))
+print(acc1)
+
+#part 2
+already_seen=tuple(banks)
+new = []
+acc2 = 0
+while tuple(new) != already_seen:
+  acc2 += 1
+  i = banks.index(max(banks))
+  temp = banks[i]
+  banks[i] = 0
+  i = (i+1)%(len(banks))
+  while(temp != 0):
+    banks[i] += 1
+    temp -= 1
+    i = (i+1)%(len(banks))
+  new = banks
+print(acc2)
